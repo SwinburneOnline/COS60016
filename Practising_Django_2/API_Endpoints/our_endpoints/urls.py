f9a991cd450e9efcf074879b1d3e0be2dd006efc
CleoHammad <102500335+CleoHammad@users.noreply.github.com>
2022-04-19 11:13:52 +0300

    Update urls.py

--- Practising_Django_2/API_Endpoints/our_endpoints/urls.py
+++ Practising_Django_2/API_Endpoints/our_endpoints/urls.py
@@ -9,10 +9,11 @@ from our_endpoints.views import get_current_server_time, upload_file, check_conn
 urlpatterns = [
     path('server_time/', get_current_server_time),
     path('upload_file/', upload_file),
-    path('status2/', check_connection),
+    path('status/', check_connection),
     path('save_json_to_file/', save_json_to_file),
     path('delete_file/', delete_file),
     path('update_file/', update_file)
+    path('check_connection/', check_connection)
 ]
 
 app_name = 'our_endpoints'
